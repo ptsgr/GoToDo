0439224f5f34730a8f80370cc4b30beba8183d61
ptsgr <egor.pats@gmail.com>
2021-05-05 17:39:03 +0300

    Add Dependency Injection

--- cmd/main.go
+++ cmd/main.go
@@ -5,11 +5,15 @@ import (
 
 	"github.com/ptsgr/GoToDo"
 	"github.com/ptsgr/GoToDo/pkg/handler"
+	"github.com/ptsgr/GoToDo/pkg/repository"
+	"github.com/ptsgr/GoToDo/pkg/service"
 )
 
 func main() {
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handler := handler.NewHandler(services)
 	srv := new(GoToDo.Server)
-	handler := new(handler.Handler)
 
 	if err := srv.Run("8080", handler.InitRoutes()); err != nil {
 		log.Fatalf("Error running http server: %s", err.Error())
