ee314d81de7fa23c8a52273a71bb036e3eaef3d8
ptsgr <egor.pats@gmail.com>
2021-05-06 09:40:29 +0300

    Add config file and viper

--- cmd/main.go
+++ cmd/main.go
@@ -7,15 +7,26 @@ import (
 	"github.com/ptsgr/GoToDo/pkg/handler"
 	"github.com/ptsgr/GoToDo/pkg/repository"
 	"github.com/ptsgr/GoToDo/pkg/service"
+	"github.com/spf13/viper"
 )
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("Error cannot initializing configs: %s", err.Error())
+	}
+
 	repos := repository.NewRepository()
 	services := service.NewService(repos)
 	handler := handler.NewHandler(services)
 	srv := new(GoToDo.Server)
 
-	if err := srv.Run("8080", handler.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("port"), handler.InitRoutes()); err != nil {
 		log.Fatalf("Error running http server: %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
